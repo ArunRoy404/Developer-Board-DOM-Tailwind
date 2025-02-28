00104b4900ccded8c443122597d85c71a907e263
ArunRoy404 <arunroy0065@gmail.com>
2025-03-01 00:49:24 +0600

    current date added

--- scripts/dev_board.js
+++ scripts/dev_board.js
@@ -3,8 +3,9 @@ document.getElementById('discover-btn').addEventListener('click',function(){
     window.location.assign('blogs.html');
 })
 
-countTask()
+
 // assigned function counter
+countTask()
 function countTask(){
     let taskCounter = 0;
     const allTaskButtons = document.querySelectorAll('.task .task-btn');
@@ -15,3 +16,17 @@ function countTask(){
     }
     setInnerText('task-counter',taskCounter)
 }
+
+
+// get date 
+getDate()
+function getDate(){
+    const time = new Date()
+    const timeList = time.toString().split(" ")
+    const day = timeList[0]
+    const MonthDateYear = timeList[1]+" "+timeList[2]+" "+timeList[3]
+
+    setInnerText('today', day)
+    setInnerText('todayDate', MonthDateYear)
+}
+
