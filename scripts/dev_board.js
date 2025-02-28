0e73afdb9709e369df36b6bba93b8eca0305c1fb
ArunRoy404 <arunroy0065@gmail.com>
2025-03-01 01:05:35 +0600

    rgb function added

--- scripts/dev_board.js
+++ scripts/dev_board.js
@@ -11,22 +11,32 @@ function countTask(){
     const allTaskButtons = document.querySelectorAll('.task .task-btn');
     for(const btn of allTaskButtons ){
         if(btn.disabled === false){
-            taskCounter++
+            taskCounter++;
         }
     }
-    setInnerText('task-counter',taskCounter)
+    setInnerText('task-counter',taskCounter);
 }
 
 
 // get date 
-getDate()
+getDate();
 function getDate(){
-    const time = new Date()
-    const timeList = time.toString().split(" ")
-    const day = timeList[0]
-    const MonthDateYear = timeList[1]+" "+timeList[2]+" "+timeList[3]
+    const time = new Date();
+    const timeList = time.toString().split(" ");
+    const day = timeList[0];
+    const MonthDateYear = timeList[1]+" "+timeList[2]+" "+timeList[3];
 
-    setInnerText('today', day)
-    setInnerText('todayDate', MonthDateYear)
+    setInnerText('today', day);
+    setInnerText('todayDate', MonthDateYear);
 }
 
+// random rgb function
+document.getElementById('rgb-btn').addEventListener('click',function(){
+    setRGB()
+})
+function setRGB(){
+    const r = Math.round(Math.random()*255);
+    const g = Math.round(Math.random()*255);
+    const b = Math.round(Math.random()*255);
+    document.querySelector('body').style.backgroundColor = 'rgba('+r+','+g+','+b+','+0.2+')'
+}
